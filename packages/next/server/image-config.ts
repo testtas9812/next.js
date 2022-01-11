ebd2f7b6dff00df7a0711228bd86dfb226b95c59
testtas9812 <96862003+testtas9812@users.noreply.github.com>
2022-01-11 20:13:39 +0530

    Update image-config.ts

--- packages/next/server/image-config.ts
+++ packages/next/server/image-config.ts
@@ -23,6 +23,7 @@ export type ImageConfigComplete = {
 
 export type ImageConfig = Partial<ImageConfigComplete>
 
+  
 export const imageConfigDefault: ImageConfigComplete = {
   deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
